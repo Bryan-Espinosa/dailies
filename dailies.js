6c165a4d6e6787197d6fb202a048b1dcede89510
manxyrisen <bmanxy@gmail.com>
2018-05-02 09:23:38 -0500

    dailies2

--- dailies.js
+++ dailies.js
@@ -18,3 +18,27 @@ function trueOrFalse(inAscOrder){
    return true;
  } else return false;
 }
+
+
+
+
+
+
+// Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value 'Fizz' instead
+// If the value is a multiple of 5: use the value 'Buzz' instead
+// If the value is a multiple of 3 & 5: use the value 'FizzBuzz' instead
+
+function fillArray(){
+  var count = 0;
+var arr = [];
+  for(i=0;i<100;i++){
+    if(arr[i]=== ""){
+      arr.push(count++);
+    }
+  } console.log(arr);
+  
+}
